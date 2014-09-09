7811f11b2f19e920c6ee505829ac44b16480f7a6
esha- <shadrin.e@gmail.com>
2014-09-09 16:50:21 +0400

    Change simple benchmarks

--- microb/benchmarks/simple.lua
+++ microb/benchmarks/simple.lua
@@ -1,25 +1,30 @@
 -- Simple Tarantool benchmark
 
+--box.cfg{}
+
 local time = require('microb.time')
 
 local BENCH_NAME = 'simple'
 local N = 10000
 
-local function insert(s)
+local function bench(s)
     for i = 1, N do
-        s:insert({i})
+        s:insert({i, i*i})
+        s:select{i}
     end
 end
 
 local function run()
     local s = box.schema.create_space(BENCH_NAME)
     s:create_index('primary', {unique = true, parts = {1, 'NUM'}})
-    local time_diff = time.diff(insert, s)
+    local time_diff = time.diff(bench, s)
     s:drop()
     local version = box.info.version
-    result = {key = BENCH_NAME..'.insert', description = 'box.insert bench', version = version, unit = 'units/milisec', size = N, time_diff = time_diff}
+    result = {key = BENCH_NAME..'.insert-select', description = 'box insert and select bench', version = version, unit = 'units/milisec', size = N, time_diff = time_diff}
     return result
 end
+
+--run()
 
 return {
 run = run
