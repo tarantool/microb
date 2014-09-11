d32d4abe18ea73074d11ecafde78dfc011f7f5b6
esha- <shadrin.e@gmail.com>
2014-09-11 17:31:50 +0400

    Add web module

--- web/init.lua
+++ web/init.lua
@@ -1,15 +1,56 @@
 -- This module for start web page with 
 -- Tarantool benchmark charts
 
-local server = require('http.server')
 local log = require('log')
+local json = require('json')
+local server = require('http.server')
+local remote = require('net.box')
 
 local APP_DIR = './web/'
+local STORAGE_HOST = '127.0.0.1'
+local STORAGE_PORT = '33011'
 
+-- For use highcharts.js (see .../templates/index.html)
+local DATA_CFG = {categories = {}, series = {{name = nil, data = {}}}}   
 
 -- Handler fo request
 
-local function handler()
+local function handler(self)
+    -- Connection to remote storage by the use box.net.box
+    local conn = remote:new(STORAGE_HOST, STORAGE_PORT)
+    
+    if not conn:ping() then
+        error('Remote storage not available or not started')
+    end
+    log.info('Start getting data')
+    
+    -- Get data results from storage and data configuration
+    local dt = DATA_CFG
+    local sel = conn.space.results:select({iterator = ALL})
+    local id = 0
+    local series = {}
+    
+    for k,v in pairs(sel) do
+        print(k,v)
+        if id == v[1] then   
+            table.insert(series.data, v[3]/v[4])
+        else
+            series = {name = v[1], data = v[3]/v[4]}
+        end
+        local i = nil
+        for x,y in pairs(dt.categories) do
+            if not y == v[2] then
+                i = 1
+                break
+            end
+        end
+        if not i then table.insert(dt.categories, v[2]) end
+        table.insert(dt.series, series) 
+    end     
+    
+    dt = json.encode(dt)
+    print(dt)
+    return self:render({ text = dt })
 end
 
 -- Start tarantool server
@@ -21,7 +62,8 @@ local function start(host, port)
     httpd = server.new(host, port, {app_dir = APP_DIR})
     log.info('Started http server at host = %s and port = %s ', host, port)
 
-    httpd:route({ path = '', file = '/index.html'}, handler)
+    httpd:route({ path = '', file = '/index.html'})
+    httpd:route({ path = '/bench'}, handler)
     httpd:start()
 end
 
