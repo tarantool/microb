9a041bc089ddc4b8e40450d749f7e0a87f137fe4
esha- <shadrin.e@gmail.com>
2014-10-08 18:14:27 +0400

    Change web module

--- web/web.lua
+++ web/web.lua
@@ -10,6 +10,7 @@ local APP_DIR = '.'
 
 local conn = nil
 
+-- Function for box-net-bo connect/reconnert
 local function remote_box(host, port)
     print ('start_remote')
     if conn == nil then
@@ -18,50 +19,71 @@ local function remote_box(host, port)
     return conn
 end
 
--- Handler fo request
+-- Handler for request
 local function handler(self)
 -- Start box-net-box connection for using storage
     local conn = conn    
     if not conn:ping() then
         error('Remote storage not available or not started')
     end
+    
     log.info('Start getting data')
-    print (conn:ping())
-    -- For use highcharts.js (see .../templates/index.html)
-    local DATA_CFG = {categories = {}, series = {}}   
+    
+    --[[####################################################
+    JSON use highcharts.js (see .../templates/index.html)
+    Example:
+    {
+        "series":[
+            {"name":"insert benchmark", "data":[111, 222]},
+            {"name":"select benchmark", "data":[333, 123]}
+        ]   
+        "categories":
+            ["version-1.6-404", "version-1.6-405"]
+    }
+    ########################################################]]-- 
+    
+    local dt = {categories = {}, series = {}}   
  
     -- Get data results from storage and data configuration
-    local dt = DATA_CFG
     local sel = conn.space.results:select({iterator = ALL})
     local id = 0
     local series = nil
     
-    for k,v in pairs(sel) do
+    if not sel[1] then
+       log.error('Storage is empty')
+    end
+
+    for _,res in ipairs(sel) do
+        
         local i = nil
+        local metric_id = res[1]
 
         -- Get benchmark metric name
-        local mname = conn.space.headers:select{v[1]}[1][3]
+        local mname = conn.space.headers:select{metric_id}[1][3]
         
-        for x,y in pairs(dt.categories) do
+        for _,version in ipairs(dt.categories) do
 
         -- Get Tarantool version 
-            print ('version in categ',y)  
-            if y == v[2] then
+            print ('version in categ',version)  
+            if version == res[2] then
                 print ('have version in categ')
                 i = 1
                 break
             end
         end
+
         if not i then
-            table.insert(dt.categories, v[2])
+            table.insert(dt.categories, res[2])
             print('Insert version in catergor')
         end
         
         -- Get result data
-        local result_data = (v[3]/v[4])*1000
+        req = res[3]
+        time = res[4] -- in milisec
+        local result_data = (req/time)*1000
 
         -- Get benchmark result
-        if id == v[1] then   
+        if id == metric_id then   
             table.insert(series.data, result_data)
         else
             if series then
@@ -71,12 +93,12 @@ local function handler(self)
             
         end
         
-        id = v[1] 
+        id = metric_id
     end     
     if series then
         table.insert(dt.series, series)
     end
-
+    
     dt = json.encode(dt)
     print(dt)
     return self:render({ text = dt })
